256b34e2a337096b7ff13b29b9f5eedf722be16f
Sploob <crayiphone@gmail.com>
2022-04-04 11:30:53 -0700

    add specs and prototypes for adding place assigning a unique ID and deleting a place object.
    co-authored-by: Evgeniya Meshuris <em@theemm.com>

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Describe Places();
 
 Test: "It should construct places object
-code: Places("Oregon", ["kitchen", "bathroom", "living room", "bedroom"], "morning", "no place like home")
-Expected Output: {"Oregon", ["kitchen", "bathroom", "living room", "bedroom"], "morning", "no place like home"}
+code: let home = new Places("home", ["kitchen", "bathroom", "living room", "bedroom"], "morning", "no place like home");
+Expected Output: home = {"home", ["kitchen", "bathroom", "living room", "bedroom"], "morning", "no place like home"}
+
+Describe Destinations.prototype.addPlace()
+Test: "It should add a place object to Destinations"
+code: Destinations.addPlace(home);
+Expected Output: Destinations.home = {"home", ["kitchen", "bathroom", "living room", "bedroom"], "morning", "no place like home"}
+
+Describe Destinations.prototype.assignId();
+Test: "it should assign an ID to a newly created place object"
+
+Described Destinations.prototype.deletePlace();
+Test: "It should delete a place object from the destinations object using the unique ID"
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
 // Business logic Destinations
 function Destinations() {
-  this.places = {}
+  this.places = {};
+  this.currentId = 0;
 }
 
+Destinations.prototype.addPlace = function(place) {
+  place.id = this.assignId();
+  this.places[place.id] = place;
+};
 
+Destinations.prototype.assignId = function() {
+  this.currentId ++;
+  return this.currentId;
+}
+
+Destinations.prototype.deletePlace = function(id) {
+  if (this.place[id] === undefined) {
+    return false;
+  }
+  delete this.contacts[id];
+  return true;
+};
 
 // business logic Places
 function Places(location, landmarks, time, notes) {
@@ -12,3 +29,19 @@ function Places(location, landmarks, time, notes) {
   this.time = time;
   this.notes = notes;
 }
+
+Places.prototype.breakdown = function() {
+  return this.landmarks.join(", ") + " time: " + this.time + " notes: " + this.notes;
+};
+
+let vacation1 = new Destinations();
+let myHome = new Places("home", ["kitchen", "bathroom", "living room", "bedroom"], "morning", "no place like home");
+vacation1.addPlace(myHome);
+
+
+$(document).ready(function() {
+  $("#destinations").append("<p>" + myHome.location.toString() + "</p>" + "<p class='hidden'>"+ myHome.breakdown() + "</p>");
+  $(this).click(function(){
+   $("p").next().toggleClass("hidden");
+  });
+});
